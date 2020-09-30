0d7083437b2b137c92d93656bfd03aaf45adaa65
TahcinMycin <72016578+TahcinMycin@users.noreply.github.com>
2020-09-30 16:35:38 +0600

    Create circle.py
    Draws a 50 pixel circle.

---
+++ circle.py
@@ -0,0 +1,8 @@
+import turtle
+
+turtle.Turtle() # Declaring Turtle Class
+turtle.speed(1) # Controlling speed(lowest)
+
+turtle.circle(50) # Draws a circle of 50 pixel radius
+
+turtle.exitonclick()
